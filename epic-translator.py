38f363163ad443a4ec7fb2726950e936e0a5e3c2
Yeetnuggets <bananazzzzz020@gmail.com>
2023-01-06 20:45:00 -0700

    Can now auto detect source language

--- epic-translator.py
+++ epic-translator.py
@@ -4,6 +4,7 @@ import images
 import pyperclip
 import winshell, os, win32com.client
 import time
+import pyttsx3
 
 
 
@@ -11,6 +12,10 @@ import time
 
 iconlol = images.icon
 desk = winshell.desktop()
+engine = pyttsx3.init()
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
 path = os.path.join(desk, 'Translator.lnk')
@@ -32,8 +37,8 @@ try:
 
 
     layout = [
-            [sg.Combo(['English', 'French','Spanish','Arabic', 'Dutch', 'Japanese', 'Russian', 'Serbian', 'Romanian', 'Bosnian', 'Finnish', 'Italian', 'German', ], font=f, key='langin', auto_size_text=True), sg.Combo(['English', 'French','Spanish','Arabic', 'Dutch', 'Japanese', 'Russian', 'Serbian', 'Romanian', 'Bosnian', 'Finnish', 'Italian', 'German', ], font=f, key='langout', pad=(283, 0))],
-            [sg.Multiline('Translation Input',key='in', size=(40, 15), enable_events=True, font=f, no_scrollbar=True, right_click_menu=['&Right', ['&Copy All', '---', '&Paste', '---', '&Swap::1', '---', '&Clear::1', '---', '&Clear All', '---', '&Help']]), sg.Button('Translate', key='butt'), sg.Multiline('Translation Output', key='out', size=(40, 15 ), enable_events=True, font=f, no_scrollbar=True,right_click_menu=['&Right',['&Copy All::copy2','---', 'Paste::2', '&Swap::1', '---', '&Clear::2', '---', '&Clear All', '---', '&Help']])],
+            [sg.Combo(['Auto','English', 'French','Spanish','Arabic', 'Dutch', 'Japanese', 'Russian', 'Serbian', 'Romanian', 'Bosnian', 'Finnish', 'Italian', 'German', ], font=f, key='langin', auto_size_text=True), sg.Combo(['English', 'French','Spanish','Arabic', 'Dutch', 'Japanese', 'Russian', 'Serbian', 'Romanian', 'Bosnian', 'Finnish', 'Italian', 'German', ], font=f, key='langout', pad=(283, 0))],
+            [sg.Multiline('Translation Input',key='in', size=(40, 15), enable_events=True, font=f, no_scrollbar=True, right_click_menu=['&Right', ['&Copy All', '---', '&Paste', '---', '&Swap::1', '---', '&Clear::1', '---', '&Clear All', '---','&TTS', '---', '&Help']]), sg.Button('Translate', key='butt'), sg.Multiline('Translation Output', key='out', size=(40, 15 ), enable_events=True, font=f, no_scrollbar=True,right_click_menu=['&Right',['&Copy All::copy2','---', 'Paste::2', '&Swap::1', '---', '&Clear::2', '---', '&Clear All', '---', '&TTS::2', '---', '&Help']])],
             [sg.Button('<-->', key='invert', pad=(309,10), size=(5,1), font=f)],
             # [sg.Image(images.cheese), sg.Image(images.cheese)]
             ]
@@ -94,7 +99,14 @@ try:
             window['out']('')
         
         elif event == 'Help':
-            sg.popup_ok("This is a crappy translator I made. If the language you want to translate isn't listed just type it in yourself and it will probably work.", font=pf)
+            sg.popup_ok("This is a crappy translator I made. If the language you want to translate isn't listed just type it in yourself and it will probably work. Note: TTS won't work on most non-latin alphabets, so don't expect it to read out something in russian. Also probably won't be the most accurate with pronounciations", font=pf)
+        
+        elif event == 'TTS':
+            talk(values['in'])
+        
+        elif event == 'TTS::2':
+            talk(values['out'])
+            
             
             
         
